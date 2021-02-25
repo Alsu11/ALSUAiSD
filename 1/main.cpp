325bef05db9b18be06cb45de34a3a75cb20c6733
alsuy <qwe@qwe.ru>
2021-02-25 22:52:38 +0300

    Initial commit

---
+++ 1/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by alsuy on 25.02.2021.
+//
+
+#include "main.h"
+#include "iostream"
+
+using namespace std;
+int main() {
+    cout << "Hello world!";
+    return 0;
+}
---
+++ 1/main.h
@@ -0,0 +1,14 @@
+//
+// Created by alsuy on 25.02.2021.
+//
+
+#ifndef HOMEWORKS_MAIN_H
+#define HOMEWORKS_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //HOMEWORKS_MAIN_H
