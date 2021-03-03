69fdd87293237be2702e1fa0a4130bab8bf44167
alsuy <qwe@qwe.ru>
2021-03-03 21:39:59 +0300

    задача 1

---
+++ homeworks/1/P1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+    std::list<int> list1  {2};
+    int n;
+    cin >> n;
+    int i = 1;
+    int k = 3;
+    while(i != n) {
+        bool f = true;
+        for(int a: list1) {
+            if(k % a == 0) {
+                f = false;
+                break;
+            }
+        }
+        if(f) {
+            list1.push_back(k);
+            i++;
+        }
+        k += 2;
+    }
+    for(int a: list1) {
+        cout << a << " ";
+    }
+    return 0;
+}
+
---
+++ homeworks/1/P1.h
@@ -0,0 +1,8 @@
+//
+// Created by alsuy on 03.03.2021.
+//
+
+#ifndef HOMEWORKS_P1_H
+#define HOMEWORKS_P1_H
+
+#endif //HOMEWORKS_P1_H
