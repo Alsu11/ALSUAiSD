cc0ec4dbcbff17f4728620577a518722fcaa6a97
alsuy <qwe@qwe.ru>
2021-03-15 19:51:34 +0300

    задача 2

--- homeworks/2/P2.cpp
+++ homeworks/2/P2.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 struct elem {
@@ -10,7 +10,7 @@ struct cycle {
     int len;
     int start;
 };
-
+*/
 // НАЧАЛО ФУНКЦИИ ДЛЯ СДАЧИ
 cycle find_cycle(elem* head) {
     cycle ans;
@@ -59,7 +59,7 @@ cycle find_cycle(elem* head) {
     return ans;
 }
 // КОНЕЦ ФУНКЦИИ ДЛЯ СДАЧИ
-
+/*
 elem* add(elem* head, int x) {
     elem* p = new elem;
     p->val = x;
@@ -85,3 +85,4 @@ int main() {
     cout << ans.start << " 'uuuuuuuuuuu " << ans.len << endl;
     return 0;
 }
+*/
