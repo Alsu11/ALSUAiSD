a931c4b1367bc7d8a1248afe6ae4ac88505b1940
alsuy <qwe@qwe.ru>
2021-03-17 21:14:24 +0300

    задача 2

--- homeworks/2/P2.cpp
+++ homeworks/2/P2.cpp
@@ -14,14 +14,22 @@ struct cycle {
 // НАЧАЛО ФУНКЦИИ ДЛЯ СДАЧИ
 cycle find_cycle(elem* head) {
     cycle ans;
-    ans.len=0;
-    ans.start=0;
+    ans.len=-1;
+    ans.start=-1;
     elem* ch = head;
     elem* z = head;
     bool c = false;
-    while (head && ch -> next && z -> next -> next) {
-        ch = ch->next;
-        z = z->next->next;
+    if(head == NULL) {
+        ans.len = 0;
+        return ans;
+    }
+    while (ch -> next && z -> next -> next) {
+        if(ch->next) {
+            ch = ch->next;
+        }
+        if(z->next->next) {
+            z = z->next->next;
+        }
         if (ch == z) {
             c = true;
             int l = 1;
@@ -44,7 +52,7 @@ cycle find_cycle(elem* head) {
             break;
         }
     }
-    if(head && !c) {
+    if(!c) {
         int r = 1;
         elem* elem = head;
         while (elem) {
@@ -52,7 +60,6 @@ cycle find_cycle(elem* head) {
             elem = elem -> next;
         }
         ans.len = r;
-        ans.start = -1;
     }
     return ans;
 }
