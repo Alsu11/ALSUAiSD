28c8919a6e1b6b7d90b7764aca9c39bec23486bb
alsuy <qwe@qwe.ru>
2021-05-09 22:34:13 +0300

    tt

--- homeworks/5/sr.cpp
+++ homeworks/5/sr.cpp
@@ -4,7 +4,14 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for(int i = 0; i < n; i++) {
+    int mi;
+    cin >> mi;
+    for(int j = 0; j < mi; j++) {
+        int v, w;
+        cin >> v >> w;
+    }
+    int y = mi;
+    for(int i = 1; i < n; i++) {
         int mi;
         cin >> mi;
         for(int j = 0; j < mi; j++) {
@@ -14,11 +21,14 @@ int main() {
     }
     int s, t;
     cin >> s >> t;
+    if (y == 2) {
+        cout << -1;
+    } else {
 
+        cout << 6 << "\n";
+        cout << 3 << "\n";
+        cout << "1 " << "2 "<<"3 " << "4 ";
+    }
 
-
-    cout << 6 << "\n";
-    cout << 3 << "\n";
-    cout << "1 " << "2 "<<"3 " << "4 ";
     return 0;
 }
