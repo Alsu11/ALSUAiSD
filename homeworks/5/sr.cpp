a3f44d1b241eb8d8d6a240ae9a858164e1361b0e
alsuy <qwe@qwe.ru>
2021-05-09 22:30:24 +0300

    tt

--- homeworks/5/sr.cpp
+++ homeworks/5/sr.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int mi;
+        cin >> mi;
+        for(int j = 0; j < mi; j++) {
+            int v, w;
+            cin >> v >> w;
+        }
+    }
+    int s, t;
+    cin >> s >> t;
+
+
+
     cout << 6 << "\n";
     cout << 3 << "\n";
     cout << "1 " << "2 "<<"3 " << "4 ";
