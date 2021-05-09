7aab4f66614c1cfc7e74cf1e9f96e66164091612
alsuy <qwe@qwe.ru>
2021-05-09 12:57:05 +0300

    R

--- homeworks/5/P5.cpp
+++ homeworks/5/P5.cpp
@@ -1,114 +1,87 @@
 #include <iostream>
+#include <vector>
 
-using std::cout;
-using std::endl;
-using std::cin;
+using namespace std;
 
-int count;
-int * parent;
-int * rank;
-
-void init(int n) {
-    count = n;
-    parent = new int[n];
-    rank = new int[n];
-    for (int i = 0; i < n; i++) {
-        parent[i] = i;
-        rank[i] = 0;
-    }
-}
-
-int find(int x) {
-    if (parent[x] == x)
-        return x;
-    else {
-        int y = find(parent[x]);
-        parent[x] = y;
-        return y;
-    }
-}
-
-void join(int x, int y) {
-    int i = find(x);
-    int j = find(y);
-    if (i == j) return;
-    if (rank[i] < rank[j]) {
-        parent[i] = j;
-    } else {
-        parent[j] = i;
-        if (rank[i] == rank[j])
-            rank[i]++;
-    }
-}
-
-void quick_Sort(int arr[][3], int fst, int last) {
-    int i, j, pivot, tmp;
-    if (fst < last) {
-        pivot = fst;
-        i = fst;
-        j = last;
-        while (i < j) {
-            while (arr[i][2] <= arr[pivot][2] && i < last)
-                i++;
-            while (arr[j][2] > arr[pivot][2])
-                j--;
-            if (i < j) {
-                tmp = arr[i][2];
-                arr[i][2] = arr[j][2];
-                arr[j][2] = tmp;
-                tmp = arr[i][1];
-                arr[i][1] = arr[j][1];
-                arr[j][1] = tmp;
-                tmp = arr[i][0];
-                arr[i][0] = arr[j][0];
-                arr[j][0] = tmp;
-            }
-        }
-        tmp = arr[pivot][2];
-        arr[pivot][2] = arr[j][2];
-        arr[j][2] = tmp;
-        tmp = arr[pivot][1];
-        arr[pivot][1] = arr[j][1];
-        arr[j][1] = tmp;
-        tmp = arr[pivot][0];
-        arr[pivot][0] = arr[j][0];
-        arr[j][0] = tmp;
-        quick_Sort(arr, fst, j - 1);
-        quick_Sort(arr, j + 1, last);
-    }
-}
+struct adjacency {
+    int vertex;
+    int weight;
+};
 
 int main() {
-    int m, n;
-    cin >> n;
-    cin >> m;
-
-    int arr[m][3];
-    init(n);
-    for (int i = 0; i < m; i++) {
-        int a, b, weight;
-        cin >> a;
-        cin >> b;
-        cin >> weight;
-        arr[i][0] = a - 1;
-        arr[i][1] = b - 1;
-        arr[i][2] = weight;
-    }
-    quick_Sort(arr, 0, m - 1);
-
-    int s = 0;
-    int k = 0;
-    for (int i = 0; i < m; i++) {
-        if (find(arr[i][0]) != find(arr[i][1])) {
-            s += arr[i][2];
-            join(arr[i][0], arr[i][1]);
-            k++;
+    int n;
+    cin >> n; // вводим количетсов рёбер
+    vector<vector<adjacency>> list;
+    for (int i = 0; i < n; i++) {
+        int mi;
+        cin >> mi; // вводим степень вершины
+        vector<adjacency> related_tops; // вектор, где хранятся смежные данной вершине вершины
+        list.push_back(related_tops);
+        for (int j = 0; j < mi; j++) {
+            adjacency a{};
+            cin >> a.vertex >> a.weight; // вводим смежную вершину и длину между вершинами
+            a.vertex--;
+            list[i].push_back(a);
         }
     }
-    if (k == n - 1) {
-        cout << s;
-    } else {
+
+    int s, t;
+    cin >> s >> t;
+    s--;
+    t--;
+
+    int len [n];
+    for(int i = 0; i < n; i++) {
+        len[i] = 2147483647;
+    }
+    len[s] = 0;
+
+    bool wa [n];
+    int pr [n];
+
+    for (int i = 0; i < n; i++) {
+        int ver = -1;
+
+        for (int j = 0; j < n; j++) {
+            if (!wa[j] && (ver == -1 || len[j] < len[ver])) {
+                ver = j;
+            }
+        }
+
+        if (len[ver] == 2147483647) {
+            break;
+        }
+
+        wa[ver] = true;
+
+        for (int j = 0; j < list[ver].size(); j++) {
+            int vertex = list[ver][j].vertex;
+            int weight = list[ver][j].weight;
+            if (len[ver] + weight < len[vertex]) {
+                len[vertex] = len[ver] + weight;
+                pr[vertex] = ver;
+            }
+        }
+    }
+
+    vector <int> p;
+
+    int i = t;
+    while(i != s) {
+        p.push_back(i);
+        i = pr[i];
+    }
+    p.push_back(s);
+
+    if (len[t] == 2147483647) {
         cout << -1;
+    }
+    else {
+        cout << len[t] << endl; // длина кротчайшего
+        cout << p.size() - 1 << endl; // количество ребер в пути
+        for (int i = p.size() - 1; i >= 0; i--) {
+            cout << p[i] + 1 << " ";
+        }
     }
     return 0;
 }
