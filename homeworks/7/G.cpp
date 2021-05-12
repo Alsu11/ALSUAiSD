0a42e39fde652e027710df382b0347d9d6a80a82
alsuy <qwe@qwe.ru>
2021-05-13 01:09:16 +0300

    код для измерения

---
+++ homeworks/7/G.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    int n,s;
+   // cout << "количество номиналов\n";
+    cin >> n;
+    //cout << " write s \n";
+    cin >> s;
+    int tabl[n+1][s+1];
+    int wi [n+1];
+    wi[0] = 0;
+    tabl[0][0] = 0;
+    for(int i = 1; i < n+1; i++) {
+        int w;
+        //cout << "write wi \n";
+        cin >> w;
+        wi[i] = w;
+        tabl[i][0] = 0;
+    }
+    for(int i = 1; i < s+1; i++) {
+        tabl[0][i] = 2147483647;
+    }
+
+    for(int i = 1; i < n+1; i++) {
+        for(int j = 1; j < s+1; j++) {
+           /* if(j - wi[i] >= 0) {
+                if((tabl[i][j - wi[i]] + 1) < tabl[i -1][j]) {
+                    tabl[i][j] = tabl[i][j - wi[i]] + 1;
+                } else {
+                    tabl[i][j] = tabl[i - 1][j];
+                }
+            } else {
+                tabl[i][j] = tabl[i - 1][j];
+            }*/
+           if(j - wi[i] >= 0 && (tabl[i][j - wi[i]] + 1) < tabl[i -1][j]) {
+               tabl[i][j] = tabl[i][j - wi[i]] + 1;
+           } else {
+               tabl[i][j] = tabl[i - 1][j];
+           }
+        }
+    }
+    /*for(int i = 0; i < n +1; i++) {
+        for(int j = 0; j < s +1; j++) {
+            cout << tabl[i][j] << "  ";
+        }
+        cout << "\n";
+    }*/
+    int itog [n];
+    for(int i = 0; i < n+1; i++) {
+        itog[i]=0;
+    }
+    bool f = true;
+    if(tabl[n][s] == 2147483647) {
+        f = false;
+    }
+    int i = n;
+    int j = s;
+    while (tabl[i][j] != 0) {
+        if(tabl[i][j] == tabl[i - 1][j]) {
+            i--;
+        } else {
+            j = j - wi[i];
+            itog[i-1]++;
+        }
+    }
+    if(f) {
+        for(int i : itog) {
+            cout << i << " ";
+        }
+    } else {
+        cout << -1;
+    }
+    return 0;
+}
