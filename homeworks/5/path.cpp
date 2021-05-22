be4d3d438674f7d32f83e4287f8a2a3686f7b7bf
alsuy <qwe@qwe.ru>
2021-05-22 14:22:03 +0300

    path

--- homeworks/5/path.cpp
+++ homeworks/5/path.cpp
@@ -12,7 +12,6 @@ int main() {
     int n;
     cin >> n; // вводим количество рёбер
     vector<vector<adjacency>> list;
-    bool one = false;
     for (int i = 0; i < n; i++) {
         int m;
         cin >> m;
@@ -24,23 +23,24 @@ int main() {
             a.vertex--;
             list[i].push_back(a);
         }
-        one = true;
     }
 
     int s, t;
-    cin >> s >> t;
+    cin >> s;
+    cin >> t;
     s--;
     t--;
 
     int big = 2147483647;
     int len [n];
-    for(int i = 0; i < n; i++) {
-        len[i] = big;
-    }
-    len[s] = 0;
-
     bool wa [n];
     int pr [n];
+
+    for(int i = 0; i < n; i++) {
+        len[i] = big;
+        wa[i] = false;
+    }
+    len[s] = 0;
 
     for (int i = 0; i < n; i++) {
         int ver = -1;
@@ -51,42 +51,39 @@ int main() {
             }
         }
 
-        if (len[ver] == big) {
-            break;
-        }
+        if(len[ver] != big) {
+            wa[ver] = true;
 
-        wa[ver] = true;
-
-        for (int j = 0; j < list[ver].size(); j++) {
-            int vertex = list[ver][j].vertex;
-            int weight = list[ver][j].weight;
-            if (len[ver] + weight < len[vertex]) {
-                len[vertex] = len[ver] + weight;
-                pr[vertex] = ver;
+            for (int j = 0; j < list[ver].size(); j++) {
+                int vertex = list[ver][j].vertex;
+                int weight = list[ver][j].weight;
+                if (len[ver] + weight < len[vertex]) {
+                    len[vertex] = len[ver] + weight;
+                    pr[vertex] = ver;
+                }
             }
         }
     }
 
-    vector <int> p;
-    int l = 1;
-    for(int i = t; i!=s; i = pr[i]) {
-        p.push_back(i);
-        l++;
-    }
-    p.push_back(s);
+    if(len[t] != big) {
 
-    if (len[t] == big) {
-        cout << -1;
-    }
-    else {
-        cout << len[t] << "\n"; // длина кротчайшего
-        cout << l - 1 << "\n"; // количество ребер в пути
+        vector <int> p;
+        int l = 1;
+        for(int i = t; i!=s; i = pr[i]) {
+            p.push_back(i);
+            l++;
+        }
+        p.push_back(s);
+
+        cout << len[t] << "\n";
+        cout << l - 1 << "\n";
         for (int i = l - 1; i >= 0; i--) {
             p[i]++;
             cout << p[i];
             cout << " ";
         }
+    } else {
+        cout << -1;
     }
-
     return 0;
 }
