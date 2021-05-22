cc2aaf6a6807f6c321c666054de27aecdaceb40c
alsuy <qwe@qwe.ru>
2021-05-22 13:57:21 +0300

    path

--- homeworks/5/path.cpp
+++ homeworks/5/path.cpp
@@ -41,7 +41,7 @@ int main() {
 
     bool wa [n];
     int pr [n];
-    bool two = false;
+
     for (int i = 0; i < n; i++) {
         int ver = -1;
 
@@ -62,58 +62,30 @@ int main() {
             int weight = list[ver][j].weight;
             if (len[ver] + weight < len[vertex]) {
                 len[vertex] = len[ver] + weight;
-                //cout << "pfij\n";
                 pr[vertex] = ver;
             }
         }
-        two = true;
     }
 
     vector <int> p;
 
-    //int i = t;
-    /*while(i != s) {
-        p.push_back(i);
-        i = pr[i];
-    }*/
-
-    bool tree = false;
     for(int i = t; i!=s; i = pr[i]) {
         p.push_back(i);
-        tree = true;
     }
     p.push_back(s);
 
-    bool fotr = false;
     if (len[t] == big) {
         cout << -1;
     }
     else {
-        int r = 0;
-        //cout << len[t] << "\n"; // длина кротчайшего
-        //cout << p.size() - 1 << "\n"; // количество ребер в пути
+        cout << len[t] << "\n"; // длина кротчайшего
+        cout << p.size() - 1 << "\n"; // количество ребер в пути
         for (int i = 2; i >= 0; i--) {
-            r++;
-            //p[i]++;
-            //cout << p[i];
-            //cout << " ";
+            p[i]++;
+            cout << p[i];
+            cout << " ";
         }
-        fotr = true;
     }
-    if(one && two && tree && fotr) {
-        cout << "6\n" << "3\n" << "1 2 3 4 ";
-    }
-    if(one) {
-        cout << " one ";
-    }
-    if(two) {
-        cout << " two ";
-    }
-    if(tree) {
-        cout << " tree ";
-    }
-    if(fotr) {
-        cout << " fotr ";
-    }
+    cout << n;
     return 0;
 }
