7092d631d83e51923406aa67a5f9ce2595552671
alsuy <qwe@qwe.ru>
2021-05-24 18:03:12 +0300

    J

--- homeworks/J/J.cpp
+++ homeworks/J/J.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    cout << -123;
-    return 0;
+    long long c, e, n;
+    cin >> c;
+    cin >> e;
+    cin >> n;
+    int e2 = pow(2,e);
+    cout << e2;
+    long long l,r; // правая и левая границы
+    l = 0;
+    r = n;
+    long long a;
+    while (true) {
+        if(l == r) {
+            a = l;
+            break;
+        }
+        cout << c;
+        long long ans;
+        cin >> ans;
+
+        if(ans < 0) {
+            a = l;
+            cout << -a;
+            break;
+        }
+
+        long long border = (l + r) / 2;
+        if(ans == 0) {
+            r = border;
+        } else {
+            l = border;
+        }
+
+        c = (c * e2) % n;
+
+    }
 }
