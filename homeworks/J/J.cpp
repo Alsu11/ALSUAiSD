c836322cd89f55a49b34207756b8177e51b9c963
alsuy <qwe@qwe.ru>
2021-05-24 20:25:03 +0300

    J

--- homeworks/J/J.cpp
+++ homeworks/J/J.cpp
@@ -7,12 +7,31 @@ long long getC(long long c, long long e, long long n) {
     return (c * e) % n;
 }
 
+long long bpow(long long x, long long e) {
+    long long count=1;
+    if (!e) return 1;
+    while (e)
+    {
+        if (e % 2 == 0)
+        {
+            e/=2;
+            x*=x;
+        }
+        else
+        {
+            e--;
+            count*=x;
+        }
+    }
+    return count;
+}
+
 int main() {
     long long c, e, n, l, r, ans, vm_c;
     cin >> c;
     cin >> e;
     cin >> n;
-    long long e2 = pow(2,e);
+    long long e2 = bpow(2,e);
     // правая и левая границы
     l = 0;
     r = n;
