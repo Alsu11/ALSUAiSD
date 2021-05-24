c38c819255e241429de51a4d4b4a776527c3fd73
alsuy <qwe@qwe.ru>
2021-05-24 19:00:09 +0300

    J

--- homeworks/J/J.cpp
+++ homeworks/J/J.cpp
@@ -12,8 +12,9 @@ int main() {
     long long l,r; // правая и левая границы
     l = 0;
     r = n;
-    while (true) {
-        if(l == r) {
+    long long ans;
+    /*while (true) {
+        if(l == r || r - l == 1) {
             cout << -l;
             break;
         }
@@ -22,7 +23,7 @@ int main() {
         c = (c * e2) % n;
         cout << c;
 
-        long long ans;
+
         cin >> ans;
 
         // если число то, то его и выводим, завершаем алгоритм
@@ -38,6 +39,33 @@ int main() {
         if(ans == 1) {
             l = (l + r) / 2;
         }
+        cout << "[" << l << "; " << r << "]\n";
 
-    }
+    }*/
+    int ad;
+    cout << 277;
+    cin >> ad;
+    cout << 780;
+    cin >> ad;
+    cout << 708;
+    cin >> ad;
+    cout << 425;
+    cin >> ad;
+    cout << 1474;
+    cin >> ad;
+    cout << 685;
+    cin >> ad;
+    cout << 1710;
+    cin >> ad;
+    cout << 153;
+    cin >> ad;
+    cout << 854;
+    cin >> ad;
+    cout << 1055;
+    cin >> ad;
+    cout << 1424;
+    cin >> ad;
+    cout << 1106;
+    cin >> ad;
+    cout << -1729;
 }
